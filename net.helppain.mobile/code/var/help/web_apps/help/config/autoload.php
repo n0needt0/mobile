c559b383f133f127cb65ca7e564fe5ae60b5c428
Andrew Yasinsky <andrew@yasinsky.com>
2013-05-13 12:26:27 -0700

    disabled mongo

--- net.helppain.mobile/code/var/help/web_apps/help/config/autoload.php
+++ net.helppain.mobile/code/var/help/web_apps/help/config/autoload.php
@@ -52,7 +52,7 @@ $autoload['packages'] = array();
 |	$autoload['libraries'] = array('database', 'session', 'xmlrpc');
 */
 
-$autoload['libraries'] = array('utils','email','database','mdb');
+$autoload['libraries'] = array('utils','email','database');
 
 /*
  | -------------------------------------------------------------------
