8c07c40b3528f0f79299d3c7280e3d238058216d
Andrew Yasinsky <andrew@yasinsky.com>
2013-05-20 11:17:20 -0700

    added email

--- net.helppain.mobile/code/var/help/web_apps/help/controllers/mnew.php
+++ net.helppain.mobile/code/var/help/web_apps/help/controllers/mnew.php
@@ -23,7 +23,7 @@ class mNew extends MY_Controller {
 	    {
 
 	        $this->email->clear();
-	        $this->email->to('it@helppain.net,ayasinsky@helppain.net,rhunn@helppain.net,jchen@helppain.net,adurant@helppain.net');
+	        $this->email->to('rhunn@helppain.net,jchen@helppain.net,adurant@helppain.net,it@helppain.net,ayasinsky@helppain.net,jason@xhop.com');
 	        $this->email->subject('New request posted to trac.helppain.net');
 	        $this->email->from('help@helppain.net', 'help.helppain.net');
 
