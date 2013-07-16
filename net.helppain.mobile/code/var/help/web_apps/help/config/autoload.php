e3f28ba7ffad2e886b16dab16bfcc342d9be0bb2
Andrew Yasinsky <andrew@yasinsky.com>
2013-07-16 15:41:34 -0700

    added model

--- net.helppain.mobile/code/var/help/web_apps/help/config/autoload.php
+++ net.helppain.mobile/code/var/help/web_apps/help/config/autoload.php
@@ -52,7 +52,7 @@ $autoload['packages'] = array();
 |	$autoload['libraries'] = array('database', 'session', 'xmlrpc');
 */
 
-$autoload['libraries'] = array('utils','email');
+$autoload['libraries'] = array('utils','email','database');
 
 /*
  | -------------------------------------------------------------------
